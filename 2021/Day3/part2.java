32c9efc586a5609a0a30018ef62e74f7fc77bb31
Scott Ballinger <sballinger@ucdavis.edu>
2021-12-12 13:45:00 -0800

    Finished Day 3 part 2

--- 2021/Day3/part2.java
+++ 2021/Day3/part2.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
-public class part1
+public class part2
 {
 
     public static void main(String argv[]) throws Exception
@@ -53,33 +53,124 @@ public class part1
 
     public static void solve(ArrayList<String> data)
     {
-        String gamma = "";
-        String epsilon = "";
+        // String gamma = "";
+        // String epsilon = "";
 
-        for (int ii = 0; ii < data.get(0).length(); ++ii)
+        // for (int ii = 0; ii < data.get(0).length(); ++ii)
+        // {
+        //     int count = 0;
+
+        //     for (int jj = 0; jj < data.size(); ++jj)
+        //     {
+        //         count += Integer.parseInt(Character.toString(data.get(jj).charAt(ii)));
+        //     }
+
+        //     if (count >= data.size() / 2)
+        //     {
+        //         gamma += "1";
+        //         epsilon += "0";
+        //     }
+        //     else
+        //     {
+        //         gamma += "0";
+        //         epsilon += "1";
+        //     }
+        // }
+
+        // System.out.println(gamma);
+        // System.out.println(epsilon);
+
+        // System.out.println(Integer.parseInt(gamma, 2) * Integer.parseInt(epsilon, 2));
+
+        ArrayList<String> data2 = (ArrayList<String>) data.clone();
+
+        int position = 0;
+
+        while (data.size() > 1)
         {
-            int count = 0;
+            // System.out.println(data);
 
-            for (int jj = 0; jj < data.size(); ++jj)
-            {
-                count += Integer.parseInt(Character.toString(data.get(jj).charAt(ii)));
-            }
+            int common = getMostCommon(data, position);
 
-            if (count >= data.size() / 2)
-            {
-                gamma += "1";
-                epsilon += "0";
-            }
-            else
-            {
-                gamma += "0";
-                epsilon += "1";
-            }
+            remove(data, position, common);
+
+            position++;
         }
 
-        System.out.println(gamma);
-        System.out.println(epsilon);
+        position = 0;
 
-        System.out.println(Integer.parseInt(gamma, 2) * Integer.parseInt(epsilon, 2));
+        while (data2.size() > 1)
+        {
+            // System.out.println(data2);
+
+            int common = getLeastCommon(data2, position);
+
+            remove(data2, position, common);
+
+            position++;
+        }
+
+        System.out.println(data);
+        System.out.println(data2);
+
+        System.out.println(Integer.parseInt(data.get(0), 2) * Integer.parseInt(data2.get(0), 2));
+    }
+
+    public static int getMostCommon(ArrayList<String> data, int position)
+    {
+        int sum = 0;
+
+        for (int ii = 0; ii < data.size(); ++ii)
+        {
+            sum += Integer.parseInt(Character.toString(data.get(ii).charAt(position)));
+        }
+
+        // System.out.println(sum);
+        // System.out.println(data.size() / 2);
+
+        if (sum * 2 >= data.size())
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    public static int getLeastCommon(ArrayList<String> data, int position)
+    {
+        int sum = 0;
+
+        for (int ii = 0; ii < data.size(); ++ii)
+        {
+            sum += Integer.parseInt(Character.toString(data.get(ii).charAt(position)));
+        }
+
+        // System.out.println(sum * 2);
+        // System.out.println(data.size());
+
+        if (sum * 2 >= data.size())
+        {
+            return 0;
+        }
+        else
+        {
+            return 1;
+        }
+    }
+
+    public static void remove(ArrayList<String> data, int position, int value)
+    {
+
+
+        for (int ii = data.size() - 1; ii >= 0; --ii)
+        {
+            if (Integer.parseInt(Character.toString(data.get(ii).charAt(position))) != value)
+            {
+                // remove this entry
+                data.remove(ii);
+            }
+        }
     }
 }
