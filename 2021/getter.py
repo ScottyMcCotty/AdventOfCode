4db9fd18ecfa96ecc98af121fba02e6b1de1977d
Scott Ballinger <sballinger@ucdavis.edu>
2021-12-12 14:45:31 -0800

    Improved getter

--- 2021/getter.py
+++ 2021/getter.py
@@ -2,11 +2,13 @@ import subprocess
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import os, sys
+from shutil import copyfile
 
 def setup(day):
     # day = str(sys.argv[1]) # old code, when only 1 day was accepted
 
-    path = str(os.getcwd()) + "/Day" + day
+    # path = str(os.getcwd()) + "/Day" + day
+    path = "Day" + day
 
     #Create folder from input
     try:
@@ -27,37 +29,46 @@ def setup(day):
     # f.close()
 
     # this might be OS specific, hopefully it works
-    subprocess.run(["copy", "part1.java", "Day" + day], shell=True)
+    # subprocess.run(["copy", "part1.java", "Day" + day], shell=True)
+    copyfile("Makefile.in", path + "/Makefile")
+    copyfile("Part1.java.in", path + "/Part1.java")
+    copyfile("Part2.java.in", path + "/Part2.java")
 
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
+    # chrome_options = Options()
+    # chrome_options.add_argument("--headless")
 
-    # Putting my session cookie in a public repository is bad practice...
-    session_cookie = {"name": "session", "value": "53616c7465645f5ff57fc2becfee8e21fbad7b7b13c800ba673107193059e3b43f82f17bd27fe133487ff0b8e6eb405b"}
+    # read the key from local.properties
+    with open("local.properties", "r") as f:
+        sessionToken = f.readlines()[0]
+
+    # create session cookie dictionary
+    # session_cookie = {"name": "session", "value": key}
 
     # initialize driver object and change the <path_to_chrome_driver> depending on your directory where your chromedriver should be
-    driver = webdriver.Chrome(chrome_options=chrome_options, executable_path="C:/Users/Scott/Downloads/chromedriver_win32/chromedriver.exe")
+    # driver = webdriver.Chrome(chrome_options=chrome_options, executable_path="C:/Users/Scott/Downloads/chromedriver_win32/chromedriver.exe")
 
     # URLs we need
-    url = "https://adventofcode.com/2021/day/"
+    # url = "https://adventofcode.com/2021/day/"
     url_input = "https://adventofcode.com/2021/day/" + day + "/input"
 
     # get request to target the site selenium is active on, add our cookies and go to input
-    driver.get(url)
-    driver.add_cookie(session_cookie)
-    driver.get(url_input)
+    # driver.get(url)
+    # driver.add_cookie(session_cookie)
+    # driver.get(url_input)
+    
+    subprocess.run(["curl", "-b", sessionToken, "-o", path + "/input.txt", url_input]) #, shell=True)
 
     # Find our text input on the input page, "pre" is our tag ID
-    content = driver.find_element_by_tag_name("pre").text
+    # content = driver.find_element_by_tag_name("pre").text
 
     # Save content to file
-    path = path + "/input.txt"
-    with open(path, "a") as f:
-        for line in content:
-            f.write(line)
+    # path = path + "/input.txt"
+    # with open(path, "a") as f:
+    #     for line in content:
+    #         f.write(line)
             
     # Remember to quit the driver
-    driver.quit()
+    # driver.quit()
 
     #Success?!
     print(str(path) + " and adjacent files created!")
